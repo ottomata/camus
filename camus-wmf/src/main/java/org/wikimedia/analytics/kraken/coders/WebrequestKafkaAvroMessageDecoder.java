790722921d5844283eff5fad3fd1b49b3f90d63a
Andrew Otto <acotto@gmail.com>
2013-08-12 12:26:21 -0400

    Adding camus-wmf project

---
+++ camus-wmf/src/main/java/org/wikimedia/analytics/kraken/coders/WebrequestKafkaAvroMessageDecoder.java
@@ -0,0 +1,95 @@
+package org.wikimedia.analytics.kraken.coders;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
+import kafka.message.Message;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DecoderFactory;
+
+import com.linkedin.camus.coders.CamusWrapper;
+import com.linkedin.camus.coders.MessageDecoder;
+import com.linkedin.camus.coders.MessageDecoderException;
+import com.linkedin.camus.schemaregistry.CachedSchemaRegistry;
+import com.linkedin.camus.schemaregistry.SchemaRegistry;
+import com.linkedin.camus.etl.kafka.coders.LatestSchemaKafkaAvroMessageDecoder;
+
+import org.apache.hadoop.io.Text;
+
+
+public class WebrequestKafkaAvroMessageDecoder extends LatestSchemaKafkaAvroMessageDecoder {
+
+
+    @Override
+    public CamusWrapper<Record> decode(byte[] payload)
+    {
+        try
+        {
+            GenericDatumReader<Record> reader = new GenericDatumReader<Record>();
+
+            Schema schema = super.registry.getLatestSchemaByTopic(super.topicName).getSchema();
+
+            reader.setSchema(schema);
+
+            return new CamusTimestampWrapper<Record>(reader.read(
+                    null,
+                    decoderFactory.jsonDecoder(
+                            schema,
+                            new String(
+                                    payload,
+                                    //Message.payloadOffset(message.magic()),
+                                    Message.MagicOffset(),
+                                    payload.length - Message.MagicOffset()
+                            )
+                    )
+            ));
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class CamusTimestampWrapper extends CamusWrapper<Record> {
+        public CamusTimestampWrapper(Record record) {
+            super(record);
+            Record header = (Record) super.getRecord().get("header");
+            if (header != null) {
+                if (header.get("server") != null) {
+                    put(new Text("server"), new Text(header.get("server").toString()));
+                }
+                if (header.get("service") != null) {
+                    put(new Text("service"), new Text(header.get("service").toString()));
+                }
+            }
+        }
+
+        @Override
+        public long getTimestamp() {
+            Record header = (Record) super.getRecord().get("header");
+            String timestampString;
+
+            if (header != null && header.get("time") != null) {
+                timestampString = (String)header.get("time");
+            } else if (super.getRecord().get("timestamp") != null) {
+                timestampString = (String)super.getRecord().get("timestamp");
+            } else {
+                return System.currentTimeMillis();
+            }
+    //        [09/Aug/2013:16:03:58 +0000]
+
+            try {
+                return (new SimpleDateFormat("[dd/MMM/yyyy:HH:mm:ss Z]").parse(timestampString).getTime());
+            } catch (NullPointerException e) {
+                    throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
---
+++ camus-wmf/src/main/java/org/wikimedia/analytics/kraken/schemaregistry/KrakenSchemaRegistry.java
@@ -0,0 +1,18 @@
+package org.wikimedia.analytics.kraken.schemaregistry;
+
+import org.apache.avro.Schema;
+
+import org.wikimedia.analytics.kraken.records.Webrequest;
+import com.linkedin.camus.schemaregistry.MemorySchemaRegistry;
+
+/**
+ * This is a little dummy registry that just uses a memory-backed schema
+ * registry to store two dummy Avro schemas. You can use this with
+ * camus.properties
+ */
+public class KrakenSchemaRegistry extends MemorySchemaRegistry<Schema> {
+	public KrakenSchemaRegistry() {
+		super();
+		super.register("varnish0", Webrequest.newBuilder().build().getSchema());
+	}
+}
